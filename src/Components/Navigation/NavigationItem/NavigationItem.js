85a5f7f208182a35c57f6a2963ffb47f91c2bb73
stanimir-georgiev <stanimir.georgiev_99@abv.bg>
2021-01-22 22:47:05 +0200

    Header markup

--- src/Components/Navigation/NavigationItem/NavigationItem.js
+++ src/Components/Navigation/NavigationItem/NavigationItem.js
@@ -1,7 +1,18 @@
 import React from "react"
+import PropTypes from "prop-types"
+import NavigationItemStyles from "./NavigationItem.module.scss"
 
-const NavigationItem = () => {
-  return null
+const NavigationItem = ({ href, text }) => {
+  return (
+    <a href={href} className={NavigationItemStyles.navigationItem}>
+      {text}
+    </a>
+  )
+}
+
+NavigationItem.propTypes = {
+  href: PropTypes.string,
+  text: PropTypes.string,
 }
 
 export default NavigationItem
