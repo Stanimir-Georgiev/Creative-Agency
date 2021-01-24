bd6a7248f57b79e7320f24dfba69eea7416bbe2d
stanimir-georgiev <stanimir.georgiev_99@abv.bg>
2021-01-24 22:29:09 +0200

    Image fallback

--- src/Components/Image/Image.js
+++ src/Components/Image/Image.js
@@ -1,8 +1,26 @@
 import React from "react"
-import ImageStyles from "Image.module.scss"
+import styles from "./Image.module.scss"
+import PropTypes from "prop-types"
+import NoImage from "../../Images/no-image-found-360x250.png"
 
-const Image = () => {
-  return null
+const Image = ({ imgSrc, imgAlt, aspectRatio }) => {
+  // A check if there is an image set before we output it below.
+  const img = imgSrc ? (
+    <img src={imgSrc} alt={imgAlt} />
+  ) : (
+    <img src={NoImage} alt="No Image" />
+  )
+  return (
+    <picture className={styles.image} style={{ paddingBottom: `${aspectRatio}%` }}>
+      {img}
+    </picture>
+  )
+}
+
+Image.PropTypes = {
+  imgSrc: PropTypes.string,
+  imgAlt: PropTypes.string,
+  aspectRatio: PropTypes.number,
 }
 
 export default Image
--- src/Components/Post/Post.js
+++ src/Components/Post/Post.js
@@ -1,21 +1,13 @@
 import React from "react"
 import styles from "./Post.module.scss"
 import Proptypes from "prop-types"
+import Image from "../../Components/Image/Image"
 
 const Post = ({ imgSrc, imgAlt, heading, description, href }) => {
-  // A check if there is an image set before we output it below.
-  const imageTag = imgSrc ? (
-    <picture className={styles.postImage}>
-      <img src={imgSrc} alt={imgAlt} />
-    </picture>
-  ) : (
-    <div className={styles.noImage}></div>
-  )
-
   return (
     <div className={styles.post}>
       <a href={href} className={styles.postLinkWrapper}>
-        {imageTag}
+        <Image imgSrc={imgSrc} imgAlt={imgAlt} aspectRatio={70} />
         <h3 className={styles.postHeading}>{heading}</h3>
         <p className={styles.postDescription}>{description}</p>
       </a>
